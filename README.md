64909f1288522a396903d6d7a5894fd89c48df64
wk-j <somnuk.wk@gmail.com>
2019-08-22 18:54:33 +0700

    Implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Action Badge
+
+[![Actions](https://github.com/wk-j/action-badge/workflows/dotnet/badge.svg)](https://github.com/wk-j/action-badge/actions)
--- src/ActionBadge/Program.fs
+++ src/ActionBadge/Program.fs
@@ -1,8 +1,49 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+type Options = {
+    Organization: string
+    Project: string
+    Action: string
+}
+
+let rec parseOptions options argv =
+    match argv with
+    | "--project" :: xs ->
+        match xs with
+        | value :: xss ->
+            parseOptions { options with Project = value } xss
+        | _ ->
+            parseOptions options xs
+    | "--organization" :: xs ->
+        match xs with
+        | value :: xss ->
+            parseOptions { options with Organization = value } xss
+        | _ ->
+            parseOptions options xs
+    | "--action" :: xs ->
+        match xs with
+        | value :: xss ->
+            parseOptions { options with Action = value } xss
+        | _ ->
+            parseOptions options xs
+    | _ ->
+        options
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let options =
+        parseOptions
+            { Organization = "wk-j"; Project= ""; Action = "" }
+            (argv |> List.ofArray)
+
+    let md =
+        """
+        [![Actions](https://github.com/{organization}/{project}/workflows/{action}/badge.svg)](https://github.com/{organization}/{project}/actions)
+        """
+            .Replace("{project}", options.Project)
+            .Replace("{organization}", options.Organization)
+            .Replace("{action}", options.Action)
+            .Trim()
+
+    printfn "%s" md
+    0
